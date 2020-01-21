bf7379184e1a13004ed4310f73fec5348adb6c19
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2020-01-21 16:55:46 +0400

    Fixing checkpoint dir for multi-tracker

--- tracker/re3_multi_tracker.py
+++ tracker/re3_multi_tracker.py
@@ -8,6 +8,7 @@ import threading
 
 import sys
 import os.path
+
 sys.path.append(os.path.abspath(os.path.join(
     os.path.dirname(__file__), os.path.pardir)))
 
@@ -27,12 +28,16 @@ from constants import MAX_TRACK_LENGTH
 
 SPEED_OUTPUT = True
 
+
 class Re3TrackerFactory(object):
-    def __init__(self, gpu_id=GPU_ID):
+    def __init__(self, checkpoint_dir=os.path.join(os.path.dirname(__file__), '..', LOG_DIR, 'checkpoints'),
+                 gpu_id=GPU_ID):
         os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu_id)
         tf.Graph().as_default()
         config = tf.ConfigProto()
         config.gpu_options.allow_growth = True
+
+        self.checkpoint_dir = checkpoint_dir
         self.sess = tf.Session(config=config)
         self.is_initialized = False
         self.tracked_data = {}
@@ -41,8 +46,7 @@ class Re3TrackerFactory(object):
     def create_tracker(self):
         tracker = Re3Tracker(self.sess, tracked_data=self.tracked_data, lock=self.lock, reuse=self.is_initialized)
         if not self.is_initialized:
-            basedir = os.path.dirname(__file__)
-            ckpt = tf.train.get_checkpoint_state(os.path.join(basedir, '..', LOG_DIR, 'checkpoints'))
+            ckpt = tf.train.get_checkpoint_state(self.checkpoint_dir)
             tf_util.restore(self.sess, ckpt.model_checkpoint_path)
             self.is_initialized = True
         return tracker
@@ -58,15 +62,14 @@ class Re3Tracker(object):
         self.batch_size = tf.placeholder(tf.int32, shape=())
 
         self.outputs, self.state1, self.state2 = network.inference(
-                self.imagePlaceholder, num_unrolls=1, batch_size=self.batch_size, train=False,
-                prevLstmState=self.prevLstmState, reuse=reuse)
+            self.imagePlaceholder, num_unrolls=1, batch_size=self.batch_size, train=False,
+            prevLstmState=self.prevLstmState, reuse=reuse)
 
         self.tracked_data = tracked_data
         self.lock = lock
 
         self.time = 0
         self.total_forward_count = -1
-
 
     # unique_id{str}: A unique id for the object being tracked.
     # image{str or numpy array}: The current image or the path to the current image.
@@ -75,7 +78,7 @@ class Re3Tracker(object):
         start_time = time.time()
 
         if type(image) == str:
-            image = cv2.imread(image)[:,:,::-1]
+            image = cv2.imread(image)[:, :, ::-1]
         else:
             image = image.copy()
 
@@ -83,7 +86,7 @@ class Re3Tracker(object):
 
         if starting_box is not None:
             lstmState = [np.zeros((1, LSTM_SIZE)) for _ in range(4)]
-            pastBBox = np.array(starting_box) # turns list into numpy array if not and copies for safety.
+            pastBBox = np.array(starting_box)  # turns list into numpy array if not and copies for safety.
             prevImage = image
             originalFeatures = None
             forwardCount = 0
@@ -93,13 +96,13 @@ class Re3Tracker(object):
             raise Exception('Unique_id %s with no initial bounding box' % unique_id)
 
         croppedInput0, pastBBoxPadded = im_util.get_cropped_input(prevImage, pastBBox, CROP_PAD, CROP_SIZE)
-        croppedInput1,_ = im_util.get_cropped_input(image, pastBBox, CROP_PAD, CROP_SIZE)
+        croppedInput1, _ = im_util.get_cropped_input(image, pastBBox, CROP_PAD, CROP_SIZE)
 
         feed_dict = {
-                self.imagePlaceholder : [croppedInput0, croppedInput1],
-                self.prevLstmState : lstmState,
-                self.batch_size : 1,
-                }
+            self.imagePlaceholder: [croppedInput0, croppedInput1],
+            self.prevLstmState: lstmState,
+            self.batch_size: 1,
+        }
         rawOutput, s1, s2 = self.sess.run([self.outputs, self.state1, self.state2], feed_dict=feed_dict)
         lstmState = [s1[0], s1[1], s2[0], s2[1]]
         if forwardCount == 0:
@@ -112,12 +115,12 @@ class Re3Tracker(object):
 
         if forwardCount > 0 and forwardCount % MAX_TRACK_LENGTH == 0:
             croppedInput, _ = im_util.get_cropped_input(image, outputBox, CROP_PAD, CROP_SIZE)
-            input = np.tile(croppedInput[np.newaxis,...], (2,1,1,1))
+            input = np.tile(croppedInput[np.newaxis, ...], (2, 1, 1, 1))
             feed_dict = {
-                    self.imagePlaceholder : input,
-                    self.prevLstmState : originalFeatures,
-                    self.batch_size : 1,
-                    }
+                self.imagePlaceholder: input,
+                self.prevLstmState: originalFeatures,
+                self.batch_size: 1,
+            }
             rawOutput, s1, s2 = self.sess.run([self.outputs, self.state1, self.state2], feed_dict=feed_dict)
             lstmState = [s1[0], s1[1], s2[0], s2[1]]
 
@@ -141,7 +144,6 @@ class Re3Tracker(object):
             print('Mean tracking speed:      %.3f FPS\n' % (self.total_forward_count / max(.00001, self.time)))
         return outputBox
 
-
     # unique_ids{list{string}}: A list of unique ids for the objects being tracked.
     # image{str or numpy array}: The current image or the path to the current image.
     # starting_boxes{None or dictionary of unique_id to 4x1 numpy array or list}: unique_ids to starting box.
@@ -152,7 +154,7 @@ class Re3Tracker(object):
         assert len(unique_ids) > 1, 'unique_ids must be at least 2 elements'
 
         if type(image) == str:
-            image = cv2.imread(image)[:,:,::-1]
+            image = cv2.imread(image)[:, :, ::-1]
         else:
             image = image.copy()
 
@@ -167,7 +169,8 @@ class Re3Tracker(object):
         for unique_id in unique_ids:
             if unique_id in starting_boxes:
                 lstmState = [np.zeros((1, LSTM_SIZE)) for _ in range(4)]
-                pastBBox = np.array(starting_boxes[unique_id]) # turns list into numpy array if not and copies for safety.
+                pastBBox = np.array(
+                    starting_boxes[unique_id])  # turns list into numpy array if not and copies for safety.
                 prevImage = image
                 originalFeatures = None
                 forwardCount = 0
@@ -180,10 +183,10 @@ class Re3Tracker(object):
                 raise Exception('Unique_id %s with no initial bounding box' % unique_id)
 
             croppedInput0, pastBBoxPadded = im_util.get_cropped_input(prevImage, pastBBox, CROP_PAD, CROP_SIZE)
-            croppedInput1,_ = im_util.get_cropped_input(image, pastBBox, CROP_PAD, CROP_SIZE)
+            croppedInput1, _ = im_util.get_cropped_input(image, pastBBox, CROP_PAD, CROP_SIZE)
             pastBBoxesPadded.append(pastBBoxPadded)
             images.extend([croppedInput0, croppedInput1])
-            for ss,state in enumerate(lstmState):
+            for ss, state in enumerate(lstmState):
                 lstmStates[ss].append(state.squeeze())
 
         lstmStateArrays = []
@@ -191,32 +194,32 @@ class Re3Tracker(object):
             lstmStateArrays.append(np.array(state))
 
         feed_dict = {
-                self.imagePlaceholder : images,
-                self.prevLstmState : lstmStateArrays,
-                self.batch_size : len(images) / 2
-                }
+            self.imagePlaceholder: images,
+            self.prevLstmState: lstmStateArrays,
+            self.batch_size: len(images) / 2
+        }
         rawOutput, s1, s2 = self.sess.run([self.outputs, self.state1, self.state2], feed_dict=feed_dict)
         outputBoxes = np.zeros((len(unique_ids), 4))
-        for uu,unique_id in enumerate(unique_ids):
+        for uu, unique_id in enumerate(unique_ids):
             lstmState, pastBBox, prevImage, originalFeatures, forwardCount = self.tracked_data[unique_id]
-            lstmState = [s1[0][[uu],:], s1[1][[uu],:], s2[0][[uu],:], s2[1][[uu],:]]
+            lstmState = [s1[0][[uu], :], s1[1][[uu], :], s2[0][[uu], :], s2[1][[uu], :]]
             if forwardCount == 0:
-                originalFeatures = [s1[0][[uu],:], s1[1][[uu],:], s2[0][[uu],:], s2[1][[uu],:]]
+                originalFeatures = [s1[0][[uu], :], s1[1][[uu], :], s2[0][[uu], :], s2[1][[uu], :]]
 
             prevImage = image
 
             # Shift output box to full image coordinate system.
             pastBBoxPadded = pastBBoxesPadded[uu]
-            outputBox = bb_util.from_crop_coordinate_system(rawOutput[uu,:].squeeze() / 10.0, pastBBoxPadded, 1, 1)
+            outputBox = bb_util.from_crop_coordinate_system(rawOutput[uu, :].squeeze() / 10.0, pastBBoxPadded, 1, 1)
 
             if forwardCount > 0 and forwardCount % MAX_TRACK_LENGTH == 0:
                 croppedInput, _ = im_util.get_cropped_input(image, outputBox, CROP_PAD, CROP_SIZE)
-                input = np.tile(croppedInput[np.newaxis,...], (2,1,1,1))
+                input = np.tile(croppedInput[np.newaxis, ...], (2, 1, 1, 1))
                 feed_dict = {
-                        self.imagePlaceholder : input,
-                        self.prevLstmState : originalFeatures,
-                        self.batch_size : 1,
-                        }
+                    self.imagePlaceholder: input,
+                    self.prevLstmState: originalFeatures,
+                    self.batch_size: 1,
+                }
                 _, s1_new, s2_new = self.sess.run([self.outputs, self.state1, self.state2], feed_dict=feed_dict)
                 lstmState = [s1_new[0], s1_new[1], s2_new[0], s2_new[1]]
 
@@ -227,7 +230,7 @@ class Re3Tracker(object):
                 # Use label if it's given
                 outputBox = np.array(starting_boxes[unique_id])
 
-            outputBoxes[uu,:] = outputBox
+            outputBoxes[uu, :] = outputBox
             self.lock.acquire()
             self.tracked_data[unique_id] = (lstmState, outputBox, image, originalFeatures, forwardCount)
             self.lock.release()
@@ -235,12 +238,9 @@ class Re3Tracker(object):
         if self.total_forward_count > 0:
             self.time += (end_time - start_time - image_read_time)
         if SPEED_OUTPUT and self.total_forward_count % 100 == 0:
-            print('Current tracking speed per object: %.3f FPS' % (len(unique_ids) / (end_time - start_time - image_read_time)))
+            print('Current tracking speed per object: %.3f FPS' % (
+                        len(unique_ids) / (end_time - start_time - image_read_time)))
             print('Current tracking speed per frame:  %.3f FPS' % (1 / (end_time - start_time - image_read_time)))
             print('Current image read speed:          %.3f FPS' % (1 / (image_read_time)))
             print('Mean tracking speed per object:    %.3f FPS\n' % (self.total_forward_count / max(.00001, self.time)))
         return outputBoxes
-
-
-
-

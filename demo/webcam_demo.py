51ae02ab461b8a34d959663f9c824157007be97d
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2020-01-21 15:16:13 +0400

    Allow using arbitrary path to the checkpoint

--- demo/webcam_demo.py
+++ demo/webcam_demo.py
@@ -1,6 +1,5 @@
 import cv2
 import argparse
-import glob
 import numpy as np
 import os
 import time
@@ -9,10 +8,6 @@ import sys
 basedir = os.path.dirname(__file__)
 sys.path.append(os.path.abspath(os.path.join(basedir, os.path.pardir)))
 from tracker import re3_tracker
-
-from re3_utils.util import drawing
-from re3_utils.util import bb_util
-from re3_utils.util import im_util
 
 from constants import OUTPUT_WIDTH
 from constants import OUTPUT_HEIGHT
@@ -26,11 +21,13 @@ boxToDraw = np.zeros(4)
 mousedown = False
 mouseupdown = False
 initialize = False
+
+
 def on_mouse(event, x, y, flags, params):
     global mousedown, mouseupdown, drawnBox, boxToDraw, initialize
     if event == cv2.EVENT_LBUTTONDOWN:
-        drawnBox[[0,2]] = x
-        drawnBox[[1,3]] = y
+        drawnBox[[0, 2]] = x
+        drawnBox[[1, 3]] = y
         mousedown = True
         mouseupdown = False
     elif mousedown and event == cv2.EVENT_MOUSEMOVE:
@@ -43,8 +40,8 @@ def on_mouse(event, x, y, flags, params):
         mouseupdown = True
         initialize = True
     boxToDraw = drawnBox.copy()
-    boxToDraw[[0,2]] = np.sort(boxToDraw[[0,2]])
-    boxToDraw[[1,3]] = np.sort(boxToDraw[[1,3]])
+    boxToDraw[[0, 2]] = np.sort(boxToDraw[[0, 2]])
+    boxToDraw[[1, 3]] = np.sort(boxToDraw[[1, 3]])
 
 
 def show_webcam(mirror=False):
@@ -62,7 +59,7 @@ def show_webcam(mirror=False):
             os.mkdir('outputs')
         tt = time.localtime()
         outputDir = ('outputs/%02d_%02d_%02d_%02d_%02d/' %
-                (tt.tm_mon, tt.tm_mday, tt.tm_hour, tt.tm_min, tt.tm_sec))
+                     (tt.tm_mon, tt.tm_mday, tt.tm_hour, tt.tm_min, tt.tm_sec))
         os.mkdir(outputDir)
         labels = open(outputDir + 'labels.txt', 'w')
     while True:
@@ -72,27 +69,27 @@ def show_webcam(mirror=False):
         origImg = img.copy()
         if mousedown:
             cv2.rectangle(img,
-                    (int(boxToDraw[0]), int(boxToDraw[1])),
-                    (int(boxToDraw[2]), int(boxToDraw[3])),
-                    [0,0,255], PADDING)
+                          (int(boxToDraw[0]), int(boxToDraw[1])),
+                          (int(boxToDraw[2]), int(boxToDraw[3])),
+                          [0, 0, 255], PADDING)
             if RECORD:
-                cv2.circle(img, (int(drawnBox[2]), int(drawnBox[3])), 10, [255,0,0], 4)
+                cv2.circle(img, (int(drawnBox[2]), int(drawnBox[3])), 10, [255, 0, 0], 4)
         elif mouseupdown:
             if initialize:
-                outputBoxToDraw = tracker.track('webcam', img[:,:,::-1], boxToDraw)
+                outputBoxToDraw = tracker.track('webcam', img[:, :, ::-1], boxToDraw)
                 initialize = False
             else:
-                outputBoxToDraw = tracker.track('webcam', img[:,:,::-1])
+                outputBoxToDraw = tracker.track('webcam', img[:, :, ::-1])
             cv2.rectangle(img,
-                    (int(outputBoxToDraw[0]), int(outputBoxToDraw[1])),
-                    (int(outputBoxToDraw[2]), int(outputBoxToDraw[3])),
-                    [0,0,255], PADDING)
+                          (int(outputBoxToDraw[0]), int(outputBoxToDraw[1])),
+                          (int(outputBoxToDraw[2]), int(outputBoxToDraw[3])),
+                          [0, 0, 255], PADDING)
         cv2.imshow('Webcam', img)
         if RECORD:
             if outputBoxToDraw is not None:
                 labels.write('%d %.2f %.2f %.2f %.2f\n' %
-                        (frameNum, outputBoxToDraw[0], outputBoxToDraw[1],
-                            outputBoxToDraw[2], outputBoxToDraw[3]))
+                             (frameNum, outputBoxToDraw[0], outputBoxToDraw[1],
+                              outputBoxToDraw[2], outputBoxToDraw[3]))
             cv2.imwrite('%s%08d.jpg' % (outputDir, frameNum), origImg)
             print('saving')
         keyPressed = cv2.waitKey(1)
@@ -102,17 +99,15 @@ def show_webcam(mirror=False):
     cv2.destroyAllWindows()
 
 
-
 # Main function
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-            description='Show the Webcam demo.')
+        description='Show the Webcam demo.')
     parser.add_argument('-r', '--record', action='store_true', default=False)
+    parser.add_argument('--checkpoint', required=True)
     args = parser.parse_args()
     RECORD = args.record
 
-    tracker = re3_tracker.Re3Tracker()
+    tracker = re3_tracker.Re3Tracker(args.checkpoint)
 
     show_webcam(mirror=True)
-
-
--- tracker/re3_tracker.py
+++ tracker/re3_tracker.py
@@ -1,17 +1,17 @@
-import cv2
 import glob
-import numpy as np
 import os
-import tensorflow as tf
 import time
-
 import sys
 import os.path
+
 sys.path.append(os.path.abspath(os.path.join(
     os.path.dirname(__file__), os.path.pardir)))
 
-from tracker import network
+import cv2
+import numpy as np
+import tensorflow as tf
 
+from tracker import network
 from re3_utils.util import bb_util
 from re3_utils.util import im_util
 from re3_utils.tensorflow_util import tf_util
@@ -26,32 +26,32 @@ from constants import MAX_TRACK_LENGTH
 
 SPEED_OUTPUT = True
 
+
 class Re3Tracker(object):
-    def __init__(self, gpu_id=GPU_ID):
+    def __init__(self, checkpoint_dir=os.path.join(os.path.dirname(__file__), '..', LOG_DIR, 'checkpoints'),
+                 gpu_id=GPU_ID):
         os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu_id)
-        basedir = os.path.dirname(__file__)
         tf.Graph().as_default()
         self.imagePlaceholder = tf.placeholder(tf.uint8, shape=(None, CROP_SIZE, CROP_SIZE, 3))
         self.prevLstmState = tuple([tf.placeholder(tf.float32, shape=(None, LSTM_SIZE)) for _ in range(4)])
         self.batch_size = tf.placeholder(tf.int32, shape=())
         self.outputs, self.state1, self.state2 = network.inference(
-                self.imagePlaceholder, num_unrolls=1, batch_size=self.batch_size, train=False,
-                prevLstmState=self.prevLstmState)
+            self.imagePlaceholder, num_unrolls=1, batch_size=self.batch_size, train=False,
+            prevLstmState=self.prevLstmState)
         self.sess = tf_util.Session()
         self.sess.run(tf.global_variables_initializer())
-        ckpt = tf.train.get_checkpoint_state(os.path.join(basedir, '..', LOG_DIR, 'checkpoints'))
+        ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
         if ckpt is None:
             raise IOError(
-                    ('Checkpoint model could not be found. '
-                    'Did you download the pretrained weights? '
-                    'Download them here: http://bit.ly/2L5deYF and read the Model section of the Readme.'))
+                ('Checkpoint model could not be found. '
+                 'Did you download the pretrained weights? '
+                 'Download them here: http://bit.ly/2L5deYF and read the Model section of the Readme.'))
         tf_util.restore(self.sess, ckpt.model_checkpoint_path)
 
         self.tracked_data = {}
 
         self.time = 0
         self.total_forward_count = -1
-
 
     # unique_id{str}: A unique id for the object being tracked.
     # image{str or numpy array}: The current image or the path to the current image.
@@ -60,7 +60,7 @@ class Re3Tracker(object):
         start_time = time.time()
 
         if type(image) == str:
-            image = cv2.imread(image)[:,:,::-1]
+            image = cv2.imread(image)[:, :, ::-1]
         else:
             image = image.copy()
 
@@ -68,7 +68,7 @@ class Re3Tracker(object):
 
         if starting_box is not None:
             lstmState = [np.zeros((1, LSTM_SIZE)) for _ in range(4)]
-            pastBBox = np.array(starting_box) # turns list into numpy array if not and copies for safety.
+            pastBBox = np.array(starting_box)  # turns list into numpy array if not and copies for safety.
             prevImage = image
             originalFeatures = None
             forwardCount = 0
@@ -78,13 +78,13 @@ class Re3Tracker(object):
             raise Exception('Unique_id %s with no initial bounding box' % unique_id)
 
         croppedInput0, pastBBoxPadded = im_util.get_cropped_input(prevImage, pastBBox, CROP_PAD, CROP_SIZE)
-        croppedInput1,_ = im_util.get_cropped_input(image, pastBBox, CROP_PAD, CROP_SIZE)
+        croppedInput1, _ = im_util.get_cropped_input(image, pastBBox, CROP_PAD, CROP_SIZE)
 
         feed_dict = {
-                self.imagePlaceholder : [croppedInput0, croppedInput1],
-                self.prevLstmState : lstmState,
-                self.batch_size : 1,
-                }
+            self.imagePlaceholder: [croppedInput0, croppedInput1],
+            self.prevLstmState: lstmState,
+            self.batch_size: 1,
+        }
         rawOutput, s1, s2 = self.sess.run([self.outputs, self.state1, self.state2], feed_dict=feed_dict)
         lstmState = [s1[0], s1[1], s2[0], s2[1]]
         if forwardCount == 0:
@@ -97,12 +97,12 @@ class Re3Tracker(object):
 
         if forwardCount > 0 and forwardCount % MAX_TRACK_LENGTH == 0:
             croppedInput, _ = im_util.get_cropped_input(image, outputBox, CROP_PAD, CROP_SIZE)
-            input = np.tile(croppedInput[np.newaxis,...], (2,1,1,1))
+            input = np.tile(croppedInput[np.newaxis, ...], (2, 1, 1, 1))
             feed_dict = {
-                    self.imagePlaceholder : input,
-                    self.prevLstmState : originalFeatures,
-                    self.batch_size : 1,
-                    }
+                self.imagePlaceholder: input,
+                self.prevLstmState: originalFeatures,
+                self.batch_size: 1,
+            }
             rawOutput, s1, s2 = self.sess.run([self.outputs, self.state1, self.state2], feed_dict=feed_dict)
             lstmState = [s1[0], s1[1], s2[0], s2[1]]
 
@@ -123,7 +123,6 @@ class Re3Tracker(object):
             print('Mean tracking speed:      %.3f FPS\n' % (self.total_forward_count / max(.00001, self.time)))
         return outputBox
 
-
     # unique_ids{list{string}}: A list of unique ids for the objects being tracked.
     # image{str or numpy array}: The current image or the path to the current image.
     # starting_boxes{None or dictionary of unique_id to 4x1 numpy array or list}: unique_ids to starting box.
@@ -134,7 +133,7 @@ class Re3Tracker(object):
         assert len(unique_ids) > 1, 'unique_ids must be at least 2 elements'
 
         if type(image) == str:
-            image = cv2.imread(image)[:,:,::-1]
+            image = cv2.imread(image)[:, :, ::-1]
         else:
             image = image.copy()
 
@@ -149,7 +148,8 @@ class Re3Tracker(object):
         for unique_id in unique_ids:
             if unique_id in starting_boxes:
                 lstmState = [np.zeros((1, LSTM_SIZE)) for _ in range(4)]
-                pastBBox = np.array(starting_boxes[unique_id]) # turns list into numpy array if not and copies for safety.
+                pastBBox = np.array(
+                    starting_boxes[unique_id])  # turns list into numpy array if not and copies for safety.
                 prevImage = image
                 originalFeatures = None
                 forwardCount = 0
@@ -160,10 +160,10 @@ class Re3Tracker(object):
                 raise Exception('Unique_id %s with no initial bounding box' % unique_id)
 
             croppedInput0, pastBBoxPadded = im_util.get_cropped_input(prevImage, pastBBox, CROP_PAD, CROP_SIZE)
-            croppedInput1,_ = im_util.get_cropped_input(image, pastBBox, CROP_PAD, CROP_SIZE)
+            croppedInput1, _ = im_util.get_cropped_input(image, pastBBox, CROP_PAD, CROP_SIZE)
             pastBBoxesPadded.append(pastBBoxPadded)
             images.extend([croppedInput0, croppedInput1])
-            for ss,state in enumerate(lstmState):
+            for ss, state in enumerate(lstmState):
                 lstmStates[ss].append(state.squeeze())
 
         lstmStateArrays = []
@@ -171,32 +171,32 @@ class Re3Tracker(object):
             lstmStateArrays.append(np.array(state))
 
         feed_dict = {
-                self.imagePlaceholder : images,
-                self.prevLstmState : lstmStateArrays,
-                self.batch_size : len(images) / 2
-                }
+            self.imagePlaceholder: images,
+            self.prevLstmState: lstmStateArrays,
+            self.batch_size: len(images) / 2
+        }
         rawOutput, s1, s2 = self.sess.run([self.outputs, self.state1, self.state2], feed_dict=feed_dict)
         outputBoxes = np.zeros((len(unique_ids), 4))
-        for uu,unique_id in enumerate(unique_ids):
+        for uu, unique_id in enumerate(unique_ids):
             lstmState, pastBBox, prevImage, originalFeatures, forwardCount = self.tracked_data[unique_id]
-            lstmState = [s1[0][[uu],:], s1[1][[uu],:], s2[0][[uu],:], s2[1][[uu],:]]
+            lstmState = [s1[0][[uu], :], s1[1][[uu], :], s2[0][[uu], :], s2[1][[uu], :]]
             if forwardCount == 0:
-                originalFeatures = [s1[0][[uu],:], s1[1][[uu],:], s2[0][[uu],:], s2[1][[uu],:]]
+                originalFeatures = [s1[0][[uu], :], s1[1][[uu], :], s2[0][[uu], :], s2[1][[uu], :]]
 
             prevImage = image
 
             # Shift output box to full image coordinate system.
             pastBBoxPadded = pastBBoxesPadded[uu]
-            outputBox = bb_util.from_crop_coordinate_system(rawOutput[uu,:].squeeze() / 10.0, pastBBoxPadded, 1, 1)
+            outputBox = bb_util.from_crop_coordinate_system(rawOutput[uu, :].squeeze() / 10.0, pastBBoxPadded, 1, 1)
 
             if forwardCount > 0 and forwardCount % MAX_TRACK_LENGTH == 0:
                 croppedInput, _ = im_util.get_cropped_input(image, outputBox, CROP_PAD, CROP_SIZE)
-                input = np.tile(croppedInput[np.newaxis,...], (2,1,1,1))
+                input = np.tile(croppedInput[np.newaxis, ...], (2, 1, 1, 1))
                 feed_dict = {
-                        self.imagePlaceholder : input,
-                        self.prevLstmState : originalFeatures,
-                        self.batch_size : 1,
-                        }
+                    self.imagePlaceholder: input,
+                    self.prevLstmState: originalFeatures,
+                    self.batch_size: 1,
+                }
                 _, s1_new, s2_new = self.sess.run([self.outputs, self.state1, self.state2], feed_dict=feed_dict)
                 lstmState = [s1_new[0], s1_new[1], s2_new[0], s2_new[1]]
 
@@ -207,13 +207,14 @@ class Re3Tracker(object):
                 # Use label if it's given
                 outputBox = np.array(starting_boxes[unique_id])
 
-            outputBoxes[uu,:] = outputBox
+            outputBoxes[uu, :] = outputBox
             self.tracked_data[unique_id] = (lstmState, outputBox, image, originalFeatures, forwardCount)
         end_time = time.time()
         if self.total_forward_count > 0:
             self.time += (end_time - start_time - image_read_time)
         if SPEED_OUTPUT and self.total_forward_count % 100 == 0:
-            print('Current tracking speed per object: %.3f FPS' % (len(unique_ids) / (end_time - start_time - image_read_time)))
+            print('Current tracking speed per object: %.3f FPS' % (
+                        len(unique_ids) / (end_time - start_time - image_read_time)))
             print('Current tracking speed per frame:  %.3f FPS' % (1 / (end_time - start_time - image_read_time)))
             print('Current image read speed:          %.3f FPS' % (1 / (image_read_time)))
             print('Mean tracking speed per object:    %.3f FPS\n' % (self.total_forward_count / max(.00001, self.time)))
@@ -231,13 +232,13 @@ class CopiedRe3Tracker(Re3Tracker):
             with tf.device('/gpu:' + str(gpu)):
                 with tf.variable_scope(network_scope):
                     self.outputs, self.state1, self.state2 = network.inference(
-                            self.imagePlaceholder, num_unrolls=1, batch_size=self.batch_size, train=False,
-                            prevLstmState=self.prevLstmState)
+                        self.imagePlaceholder, num_unrolls=1, batch_size=self.batch_size, train=False,
+                        prevLstmState=self.prevLstmState)
         else:
             with tf.variable_scope(network_scope):
                 self.outputs, self.state1, self.state2 = network.inference(
-                        self.imagePlaceholder, num_unrolls=1, batch_size=self.batch_size, train=False,
-                        prevLstmState=self.prevLstmState)
+                    self.imagePlaceholder, num_unrolls=1, batch_size=self.batch_size, train=False,
+                    prevLstmState=self.prevLstmState)
         local_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=network_scope)
         self.sync_op = self.sync_from(copy_vars, local_vars)
 
@@ -253,9 +254,7 @@ class CopiedRe3Tracker(Re3Tracker):
     def sync_from(self, src_vars, dst_vars):
         sync_ops = []
         with tf.name_scope('Sync'):
-            for(src_var, dst_var) in zip(src_vars, dst_vars):
+            for (src_var, dst_var) in zip(src_vars, dst_vars):
                 sync_op = tf.assign(dst_var, src_var)
                 sync_ops.append(sync_op)
         return tf.group(*sync_ops)
-
-
